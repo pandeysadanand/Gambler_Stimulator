c39497a77e1e06d97a7ebfb3008f86fa436564ce
sadanand pandey <pandeysadanad75@gmail.com>
2022-05-24 17:20:39 +0530

    Added: resign stake on win

--- src/main/java/com/bl/gamblerStimulator/GamblerStimulator.java
+++ src/main/java/com/bl/gamblerStimulator/GamblerStimulator.java
@@ -7,16 +7,30 @@ package com.bl.gamblerStimulator;
 public class GamblerStimulator {
 
     public static final double INITIAL_STAKE = 100;
-    public static final double STAKE_BET = 100;
+    public static final double STAKE_BET = 1;
     public static int stake = 0;
 
     public static void result() {
-        if (Math.random() < 0.5) {
+        boolean resign = false;
+        while (resign == false){
+            if (Math.random() < 0.5) {
             stake++;
             System.out.println("Win");
         } else {
             stake--;
             System.out.println("Lose");
+        }
+            if (stake >= INITIAL_STAKE + (INITIAL_STAKE * 0.5) || stake <= INITIAL_STAKE / 2) {
+                resign = true;
+                System.out.println(stake);
+            }
+        }
+    }
+    public static boolean calculation(){
+        if ((int)Math.floor(Math.random()*10)%2 == 1) {
+            return true;
+        } else {
+            return false;
         }
     }
     public static void main(String[] args) {

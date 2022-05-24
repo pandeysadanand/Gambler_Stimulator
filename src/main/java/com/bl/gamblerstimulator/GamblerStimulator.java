10c7989185590d955560e5eb37509ed4da2dcc9d
sadanand pandey <pandeysadanad75@gmail.com>
2022-05-24 16:47:26 +0530

    Added: welcome file

---
+++ src/main/java/com/bl/gamblerstimulator/GamblerStimulator.java
@@ -0,0 +1,17 @@
+package com.bl.gamblerstimulator;
+
+/**
+ * @author : Sadanand Pandey
+ * Displaying welcome message
+ * 
+ */
+
+public class GamblerStimulator {
+    public static final double initialStake = 100;
+    public static final double stakeBet = 1;
+    public static void main(String[] args) {
+        System.out.println("------------♥-Welcome to Gambler Stimulator Game-♥---------");
+
+        }
+    }
+
